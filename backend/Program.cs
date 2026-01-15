76f28ad22f7f2e657dc66dcaeac9c806cde9b913
Rud3us <rodionpylnev@yandex.ru>
2026-01-15 23:06:18 +0300

    Вообщем, мало того, что я затупил с комметами, я закасепорил ветку и пришлось её сносить...

---
+++ backend/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace MyNamespace;
+
+internal static class Program
+{
+    static void Main()
+    {
+        while (true)
+        {
+            Console.Write("Введите Имя: ");
+            Console.WriteLine($"Привет, {Console.ReadLine()}\n{DateTime.Now}\nХотите продолжить? (y/n): ");
+            if (Console.ReadLine() == "n") break;
+        }
+    } 
+}
